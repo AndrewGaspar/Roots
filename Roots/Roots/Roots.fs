7b7fa1db69afc24b8fc4bcd4f53ee57e297989af
AndrewGaspar <andrew.james.gaspar@gmail.com>
2013-09-22 00:34:10 -0500

    Completed with Newton's method and Secant method, as well as comments and basic tests

--- Roots/Roots/Roots.fs
+++ Roots/Roots/Roots.fs
@@ -2,6 +2,7 @@
 
 module Roots =
 
+    // An exception type that is raised when it is not possible to find a root
     type RootNotFoundError(lower: float, upper: float, fLower: float, fUpper: float, iterations: int) =
         inherit System.Exception(sprintf "No root between f(%f) = %f and f(%f) = %f in %d iterations" lower fLower upper fUpper iterations)
         let lower = lower 
@@ -10,31 +11,76 @@ module Roots =
         let fUpper = fUpper
         let iterations = iterations
 
-    let private updateBounds lower upper middle fLower fUpper fMiddle iteration =
-        if fLower < 0.0 && fUpper > 0.0 then
-            if fMiddle < 0.0 then (middle, upper, fMiddle, fUpper)
-            else (lower, middle, fLower, fMiddle)
-        else if fLower > 0.0 && fUpper < 0.0 then
-            if fMiddle < 0.0 then (lower, middle, fLower, fMiddle)
-            else (middle, upper, fMiddle, fUpper)
-        else raise(new RootNotFoundError(lower, upper, fLower, fUpper, iteration))
+    // An exception type that is raised when the method used takes more iterations than the specified max iterations
+    type ExhaustedIterationsError(guess: float, fGuess: float, iterations: int) =
+        inherit System.Exception(sprintf "No root found in %d iterations - Final guess: f(%f) = %f" iterations guess fGuess)
+        let guess = guess
+        let fGuess = fGuess
+        let iterations = iterations
 
+    // Returns new bounds that reflect the values of lower bound, the upper bound, and the calculated guess value
+    // Raises an exception if the bounds aren't on either side of the 
+    let private updateBounds lower upper guess fLower fUpper fGuess iteration =
+        if fLower <= 0.0 && fUpper >= 0.0 then // lower bound is negative and upper bound is positive
+            if fGuess < 0.0 then (guess, upper, fGuess, fUpper) // guess is the new lower bound
+            else (lower, guess, fLower, fGuess) // guess is the new upper bound
+        else if fLower >= 0.0 && fUpper <= 0.0 then // lower bound is positive and upper bound is negative
+            if fGuess < 0.0 then (lower, guess, fLower, fGuess) // guess is the new upper bound
+            else (guess, upper, fGuess, fUpper) // guess is the new lower bound
+        else raise(new RootNotFoundError(lower, upper, fLower, fUpper, iteration)) // both bounds are on the same side of 0
+
+    // Searches for the roots of a function by bisecting a range with each iteration until the function
+    // returns a value for the midpoint of that range within a certain accuracy from 0.
     let bisectionDebug debug f lower upper accuracy =
         let rec bisectionCalc lower upper fLower fUpper iter =
             let midpoint = (lower+upper)/2.0 // calculate the midpoint
             let acc = (upper-lower)/2.0 // calculate the accuracy
 
-            if acc < accuracy then midpoint // return the midpoint if the desired accuracy is achieved
+            let fMidpoint = f midpoint
+            debug (iter,lower,upper,midpoint,fLower,fUpper,fMidpoint); // <- for Numerical Analysis assignment
+
+            if acc < accuracy || fMidpoint = 0.0 then midpoint // return the midpoint if the desired accuracy is achieved
             else
-                let fMidpoint = f midpoint
-                debug (iter,lower,upper,midpoint,fLower,fUpper,fMidpoint);
-                if fMidpoint = 0.0 then midpoint
-                else
-                    let (newLower, newUpper, newFLower, newFUpper) =
-                        updateBounds lower upper midpoint fLower fUpper fMidpoint iter
+                // Update the bounds based on current boundsd and calculations
+                let (newLower, newUpper, newFLower, newFUpper) =
+                    updateBounds lower upper midpoint fLower fUpper fMidpoint iter
 
-                    bisectionCalc newLower newUpper newFLower newFUpper (iter + 1)
+                // Recursively call bisectionCalc with new bounds
+                bisectionCalc newLower newUpper newFLower newFUpper (iter + 1)
 
-        bisectionCalc lower upper (f lower) (f upper) 0
+        let (fLower, fUpper) = (f lower, f upper)
+        // If the function evaluates the initial values on the same side of zero, immediately raise an exception
+        if (fUpper < 0.0 && fLower < 0.0) || (fUpper > 0.0 && fLower > 0.0) then raise(new RootNotFoundError(lower, upper, fLower, fUpper, 0));
+        // Initiate first call to recursive function bisectionCalc
+        bisectionCalc lower upper fLower fUpper 0
 
     let bisection = bisectionDebug (fun _ -> ())
+
+    // Used for both Newton's method and Secant method
+    let newtonGeneral debug f f' (p1,p0) accuracy maxIters =
+        let rec newtonCalc p1 p0 fP1 fP0 iter =
+            if iter = maxIters then raise(new ExhaustedIterationsError(p1, f p1, iter));
+            let f'Prior = f' p1 p0 fP1 fP0 // f' takes both prior guesses to support secant method
+            let p2 = p1 - fP1 / f'Prior // The next guess is calculated by drawing a tangent line to the previous guess and using the root of that line
+            let fP2 = f p2
+            debug (iter, p2, p1, p0, fP2, fP1, fP0, f'Prior); // Mmmm.... toasty side effects - for the inspection... FOR THE INSPECTION
+            if System.Math.Abs (p2 - p1) < accuracy then p2 // If the new guess falls within a certain accuracy, return that guess
+            else newtonCalc p2 p1 fP2 fP1 (iter + 1) // Else recursive call as final statement for DAT TCO
+
+        newtonCalc p1 p0 (f p1) (f p0) 0 // Initiate recursion with starting values
+
+    // Newton's method uses the derivative function to more quickly discover the next root
+    // It draws a tangent line to the current guess and uses the root of the line as the next guess
+    // This repeats until a value within a certain accuracy is found
+    let newtonDebug debug f f' p0 
+        = newtonGeneral (fun (iter, p1, p0, _, fP1, fP0, _, f'Prior) -> debug(iter, p1, p0, fP1, fP0, f'Prior)) f (fun p1 _ _ _ -> f' p1) (p0,0.0)
+
+    let newton = newtonDebug (fun _ -> ())
+
+    // Like Newton's method, but rather than using the derivative of the function, a secant line
+    // is drawn between the two prior values to get an approximation of the derivative. Usually
+    // not as fast as Newton's, but doesn't require knowledge of the input function - nor does the
+    // function need to differentiable
+    let secantDebug debug f = newtonGeneral debug f (fun p1 p0 fP1 fP0 -> (fP1 - fP0)/(p1 - p0))
+
+    let secant = secantDebug (fun _ -> ())
--- Roots/Roots/Script.fsx
+++ Roots/Roots/Script.fsx
@@ -5,10 +5,70 @@
 open Roots
 open System
 
-let f1 x = 6.0*(System.Math.Pow(System.Math.E,x) - x)
-let f2 x = 6.5 + 3.0*x*x + 2.0*x*x*x
+let pow x n = System.Math.Pow(x,n)
+let E = System.Math.E
+
+let f1 x = 6.0*(pow E x - x)
+let f1' x = 6.0*(pow E x - 1.0)
+let f2 x = 6.5 + 2.0*(1.5 + x)*x*x
+let f2' x = 6.0*(1.0 + x)*x
 let f3 x = f1 x - f2 x
+let f3' x = f1' x - f2' x
 
-let printIterations (iteration, lower, upper, guess, fLower, fUpper, fGuess) = printfn "%d: Guess: f(%f) = %f | Lower: f(%f) = %f | Upper: f(%f) = %f" iteration guess fGuess lower fLower upper fUpper
+let printResult f x = printfn "f(%f) = %f" x (f x)
 
-f3 (Roots.bisectionDebug printIterations f3 -1.0 1.0 1e-4)
+///////////////////////////////////////////////////////////////
+////////    TESTING BISECTION   ///////////////////////////////
+///////////////////////////////////////////////////////////////
+
+let printBisection (iteration, lower, upper, guess, fLower, fUpper, fGuess) =
+    printfn "[a_%d, b_%d] = [%f, %f] | [f(a_%d), f(b_%d)] = [%f, %f]" iteration iteration lower upper iteration iteration fLower fUpper
+    printfn "c_%d = %f | f(c_%d) = %f" iteration guess iteration fGuess
+
+let bisection = Roots.bisectionDebug printBisection
+
+bisection f3 -1.0 1.0 1e-4 |> printResult f3
+
+///////////////////////////////////////////////////////////////
+////////    TESTING NEWTON  ///////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+let printNewton (iteration, p1, p0, fP1, fP0, f'Prior) = 
+    printfn "p_%d = %f | f(p_%d) = %f" (iteration+1) p1 (iteration+1) fP1
+
+let newton = Roots.newtonDebug printNewton
+
+let p33 p0 =
+    printf "\n\n"
+    printfn "p_0 = %f | f(p_0) = %f" p0 (f3 p0)
+    newton f3 f3' p0 1e-4 50 |> ignore
+
+p33 1.0
+p33 10.0
+try
+    p33 50.0
+with
+    | :? Roots.ExhaustedIterationsError as ex -> printfn "%s" ex.Message
+
+///////////////////////////////////////////////////////////////
+////////    TESTING SECANT  ///////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+let printSecant (iteration, p2, p1, p0, fP2, fP1, fP0, f'Prior) =
+    let currIter = iteration + 2
+    printfn "p_%d = %f | f(p_%d) = %f" currIter p2 currIter fP2
+
+let secant = Roots.secantDebug printSecant
+
+let p34 p1 p0 =
+    printf "\n\n"
+    printfn "p_0 = %f | f(p_0) = %f" p0 (f3 p0)
+    printfn "p_1 = %f | f(p_1) = %f" p1 (f3 p1)
+    secant f3 (p1,p0) 1e-4 50 |> ignore
+
+p34 0.99 1.0
+p34 9.9 10.0
+try
+    p34 49.5 50.0
+with
+    | :? Roots.ExhaustedIterationsError as ex -> printfn "%s" ex.Message
