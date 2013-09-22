b26be155a6390d8a09af0410ed0614874e46c4a3
AndrewGaspar <andrew.james.gaspar@gmail.com>
2013-09-21 21:42:58 -0500

    Bisection method complete

--- Roots/Roots/Roots.fs
+++ Roots/Roots/Roots.fs
@@ -1,7 +1,40 @@
 ﻿namespace Roots
 
 module Roots =
-    let bisection lower0 upper0 f =
-        let bounds = (lower0,upper0);
-        let (lower,upper) = bounds;
-        (lower+upper)/2
+
+    type RootNotFoundError(lower: float, upper: float, fLower: float, fUpper: float, iterations: int) =
+        inherit System.Exception(sprintf "No root between f(%f) = %f and f(%f) = %f in %d iterations" lower fLower upper fUpper iterations)
+        let lower = lower 
+        let upper = upper 
+        let fLower = fLower 
+        let fUpper = fUpper
+        let iterations = iterations
+
+    let private updateBounds lower upper middle fLower fUpper fMiddle iteration =
+        if fLower < 0.0 && fUpper > 0.0 then
+            if fMiddle < 0.0 then (middle, upper, fMiddle, fUpper)
+            else (lower, middle, fLower, fMiddle)
+        else if fLower > 0.0 && fUpper < 0.0 then
+            if fMiddle < 0.0 then (lower, middle, fLower, fMiddle)
+            else (middle, upper, fMiddle, fUpper)
+        else raise(new RootNotFoundError(lower, upper, fLower, fUpper, iteration))
+
+    let bisectionDebug debug f lower upper accuracy =
+        let rec bisectionCalc lower upper fLower fUpper iter =
+            let midpoint = (lower+upper)/2.0 // calculate the midpoint
+            let acc = (upper-lower)/2.0 // calculate the accuracy
+
+            if acc < accuracy then midpoint // return the midpoint if the desired accuracy is achieved
+            else
+                let fMidpoint = f midpoint
+                debug (iter,lower,upper,midpoint,fLower,fUpper,fMidpoint);
+                if fMidpoint = 0.0 then midpoint
+                else
+                    let (newLower, newUpper, newFLower, newFUpper) =
+                        updateBounds lower upper midpoint fLower fUpper fMidpoint iter
+
+                    bisectionCalc newLower newUpper newFLower newFUpper (iter + 1)
+
+        bisectionCalc lower upper (f lower) (f upper) 0
+
+    let bisection = bisectionDebug (fun _ -> ())
--- Roots/Roots/Script.fsx
+++ Roots/Roots/Script.fsx
@@ -1,5 +1,14 @@
 ﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
-#load "PortableLibrary1.fs"
+#load "Roots.fs"
 open Roots
+open System
+
+let f1 x = 6.0*(System.Math.Pow(System.Math.E,x) - x)
+let f2 x = 6.5 + 3.0*x*x + 2.0*x*x*x
+let f3 x = f1 x - f2 x
+
+let printIterations (iteration, lower, upper, guess, fLower, fUpper, fGuess) = printfn "%d: Guess: f(%f) = %f | Lower: f(%f) = %f | Upper: f(%f) = %f" iteration guess fGuess lower fLower upper fUpper
+
+f3 (Roots.bisectionDebug printIterations f3 -1.0 1.0 1e-4)
